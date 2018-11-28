d8323dbf8d804105be46f1d91bf478992240b9a1
Kayla Cross <kac288@georgetown.edu>
2018-11-27 21:47:31 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+voters = Voter.create([
+  {username: "kayla@kayla.com", password_digest: "supersecure", age: 25, dollars: 1000, admin: true},
+  {username: "sean@sean.com", password_digest: "wethebestmusic", age: 25, dollars: 10000, admin: false}
+])
+
+politicians = Politician.create([
+  {name: "Bernie Sanders", party: "Independent", dollars: 0},
+  {name: "Tim Kaine", party: "Democrat", dollars: 0},
+  {name: "Susan Collins", party: "Republican", dollars: 0}
+])
