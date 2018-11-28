41bddfef2b996a775a803804f56ff8e56ba7b4fa
Kayla Cross <kac288@georgetown.edu>
2018-11-27 22:03:23 -0500

    built out backbone of voters controller

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -1,2 +1,31 @@
 class VotersController < ApplicationController
+  #before_action :private_method, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @voters = Voter.all
+  end
+
+  def show
+    @voter = Voter.find_by_id(params[:id])
+  end
+
+  def new
+    @voter = Voter.new
+  end
+
+  def create
+    @voter = Voter.new(voter_params)
+    if @voter.save
+      #log in and redirect
+    else #prompt user to correct errors
+      render :new
+    end
+  end
+
+  private
+
+    def voter_params
+      params.require("voter").permit("username", "password_digest", "party", "age", "dollars", "admin")
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :donations
   resources :politicians
-  resources :voters
+  resources :voters #TODO --> nest donations under voters
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
